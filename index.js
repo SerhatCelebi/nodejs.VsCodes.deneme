8646683334268e679501a9314c9d3be81f6e69b6
SerhatCelebi <serhat6701@gmail.com>
2024-02-14 21:21:36 +0300

    deneme2

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var http = require("http");
-var fs = require("fs");
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get("/", (req, res) => {
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
+app.set("view engine", "ejs");
+app.use("/", function (req, res) {
+  res.render("index");
+});
+/*app.use("/", function (req, res) {
+  fs.readFile("index.html", (err, html) => {
+    //sleep(20000);
+    //async.res.write(html);
+    res.send(html);
+    sleep(5000);
+    //res.end();
+  });
+});
+*/
+/*app.get("/", (req, res) => {
   if (req == "/") {
     fs.readFile("index.html", (err, html) => {
       //sleep(20000);
@@ -13,7 +29,7 @@ app.get("/", (req, res) => {
       res.end();
     });
   }
-});
+});*/
 
 app.listen(port, () => {
   console.log("node.js server at port 3000");
